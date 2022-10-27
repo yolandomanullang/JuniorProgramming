1154eeb986c1752c7d7ecc86452f7c33eeec4d9d
c181022007 <yolandomanullang@gmail.com>
2022-10-27 13:44:37 +0700

    tes commit

---
+++ src/main/java/com/cobaspring/cobaspring/repositories/CarImageRepository.java
@@ -0,0 +1,6 @@
+package com.cobaspring.cobaspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarImageRepository extends JpaRepository<CarImageRepository,Integer> {
+}
