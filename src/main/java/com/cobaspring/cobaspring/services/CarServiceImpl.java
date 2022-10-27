c017ae58c4dc7218aa2370cb1d2f60d66f277db5
c181022007 <yolandomanullang@gmail.com>
2022-10-27 14:16:58 +0700

    tes commit new

--- src/main/java/com/cobaspring/cobaspring/services/CarServiceImpl.java
+++ src/main/java/com/cobaspring/cobaspring/services/CarServiceImpl.java
@@ -3,6 +3,7 @@ package com.cobaspring.cobaspring.services;
 import com.cobaspring.cobaspring.entities.CarEntity;
 import com.cobaspring.cobaspring.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class CarServiceImpl implements CarService{
     @Autowired
     CarRepository carRepository;
 
+
     @Override
     public CarEntity addCar(CarEntity param) {
         return   carRepository.save(param);
@@ -19,7 +21,7 @@ public class CarServiceImpl implements CarService{
 
     @Override
     public List<CarEntity> getAllCar() {
-        return carRepository.findAll();
+        return carRepository.findAll(Sort.sort(CarEntity.class));
     }
 
     @Override
